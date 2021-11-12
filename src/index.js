78224d1ddeda8f0d3afd8910c06699f6dcccb1d3
Jonathan <jonathantoledo11@gmail.com>
2021-11-12 10:58:41 -0800

    submisson for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-// write your code here
+const Url = 'http://localhost:3000/images/1'
+const Flatagram = document.querySelector('.image-container')
+const image = document.querySelector('.image');
+const likes = document.querySelector(".likes-section")
+const button = document.querySelector('.likes-button')
+
+fetch(Url)
+.then (res => res.json())
+.then(appPage);
+
+function appPage(photo){
+    const photoElement = document.querySelector('.title');
+    image src = assets/coder-dog.png
+    // Come back and change caption!
+    photoElement.textContent = "Change the caption dummy!"
+    photoElement.id = `${photo}`
+    photoElement.likes = 10
+   
+
+
+}
